6712777471342e4979e4e3ebdb5050cf37c5fb7e
fananchong <fananchong@126.com>
2018-03-01 21:20:42 +0800

    add tools/redis2go/main.go

---
+++ common.go
@@ -0,0 +1,27 @@
+package go_redis_orm
+
+type RD_Base interface {
+	Key() string
+	Value() ([]byte, error)
+	Load(dbName string) error
+	Save(dbName string) error
+	Delete(dbName string) error
+}
+
+type IClient interface {
+	Do(commandName string, args ...interface{}) (reply interface{}, err error)
+}
+
+type NewRedisType func(dbName string, addrs []string, password string, dbindex int) (IClient, error)
+
+func SetNewRedisHandler(handler NewRedisType) {
+	g_redismgr.SetNewRedisHandler(handler)
+}
+
+func CreateDB(dbName string, addrs []string, password string, dbindex int) {
+	g_redismgr.Create(dbName, addrs, password, dbindex)
+}
+
+func GetDB(dbName string) IClient {
+	return g_redismgr.Get(dbName)
+}
