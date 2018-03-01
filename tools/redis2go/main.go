d020c9a0e764c6437ac7c67e45ae13d4a9f9ad3c
fananchong <fananchong@126.com>
2018-03-01 22:48:01 +0800

    add tools/redis2go/template11.go

--- tools/redis2go/main.go
+++ tools/redis2go/main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/bitly/go-simplejson"
+)
+
+var (
+	inDir       = flag.String("input_dir", "", "Json文件目录路径")
+	outDir      = flag.String("output_dir", "", "输出目录路径")
+	packageName = flag.String("package", "", "生成Go文件的包名")
+)
+
+func main() {
+	flag.Parse()
+
+	err := filepath.Walk(*inDir, func(path string, f os.FileInfo, err error) error {
+		if f == nil {
+			return err
+		}
+		if f.IsDir() {
+			return nil
+		}
+		if strings.HasSuffix(f.Name(), ".json") == false {
+			return nil
+		}
+
+		fmt.Println("start process file =", f.Name(), "...")
+
+		var jsonStr []byte
+		jsonStr, err = ioutil.ReadFile(f.Name())
+		if err != nil {
+			return err
+		}
+		var js *simplejson.Json
+		js, err = simplejson.NewJson(jsonStr)
+		if err != nil {
+			return err
+		}
+		err = doFile(js)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
 }
---
+++ tools/redis2go/process.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/bitly/go-simplejson"
+
+func doFile(js *simplejson.Json) error {
+	return nil
+}
