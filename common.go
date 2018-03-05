352c6d926ed77f68828289b1c66dc6a9d81b9221
fananchong <fananchong@126.com>
2018-03-05 21:15:34 +0800

    CreateDB 增加返回值

--- common.go
+++ common.go
@@ -12,8 +12,8 @@ func SetNewRedisHandler(handler NewRedisType) {
 	g_redismgr.SetNewRedisHandler(handler)
 }
 
-func CreateDB(dbName string, addrs []string, password string, dbindex int) {
-	g_redismgr.Create(dbName, addrs, password, dbindex)
+func CreateDB(dbName string, addrs []string, password string, dbindex int)error {
+	return g_redismgr.Create(dbName, addrs, password, dbindex)
 }
 
 func GetDB(dbName string) IClient {
